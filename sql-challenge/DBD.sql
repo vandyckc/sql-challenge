4cbeb13ab897d6594851c03a89948a428468caa7
vandyckc <128192691+vandyckc@users.noreply.github.com>
2023-06-05 15:02:33 -0700

    challenge

---
+++ sql-challenge/DBD.sql
@@ -0,0 +1,70 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/tON4t3
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "departments" (
+    "dept_no" varchar(30)   NOT NULL,
+    "dept_name" varchar(30)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "department_employees" (
+	id serial NOT null,
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar(30)   NOT NULL);
+
+CREATE TABLE "employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" varchar(30)   NOT NULL,
+    "birth_date" varchar(30)   NOT NULL,
+    "first_name" varchar(30)   NOT NULL,
+    "last_name" varchar(30)   NOT NULL,
+    "sex" varchar(30)   NOT NULL,
+    "hire_date" varchar(30)   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+drop table if exists department_manager
+CREATE TABLE "department_manager" (
+    "dept_no" varchar   NOT NULL,
+    "emp_no" int   NOT NULL
+--     CONSTRAINT "pk_department_manager" PRIMARY KEY (
+--         "dept_no"
+--      )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id" varchar(30)   NOT NULL,
+    "title" varchar(250)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+-- ALTER TABLE "departments" ADD CONSTRAINT "fk_departments_dept_no" FOREIGN KEY("dept_no")
+-- REFERENCES "department_employees" ("dept_no");
+
+-- ALTER TABLE "department_employees" ADD CONSTRAINT "fk_department_employees_dept_no" FOREIGN KEY("dept_no")
+-- REFERENCES "department_manager" ("dept_no");
+
+-- ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_no" FOREIGN KEY("emp_no")
+-- REFERENCES "salaries" ("emp_no");
+
+-- ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+-- REFERENCES "titles" ("title");
+
+-- ALTER TABLE "department_manager" ADD CONSTRAINT "fk_department_manager_emp_no" FOREIGN KEY("emp_no")
+-- REFERENCES "employees" ("emp_no");
+
